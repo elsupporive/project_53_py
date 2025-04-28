3f7e53cd47e59ba932ddecd470b44db4d53b5e60
Elvira Giniatullina <elvirasupporive@gmail.com>
2025-04-27 18:57:54 -0600

    class Contact was added

---
+++ contact.py
@@ -0,0 +1,24 @@
+class Contact:
+    def __init__(self, firstName, lastName, nickname, company, address, phone_home,
+                            phone_mobile, phone_work, fax, email, email2, email3,
+                            webpage, title, bday, bmonth, byear, an_day, an_month, an_year):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.phone_home = phone_home
+        self.phone_mobile = phone_mobile
+        self.phone_work = phone_work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.webpage = webpage
+        self.title = title
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.an_day = an_day
+        self.an_month = an_month
+        self.an_year = an_year
