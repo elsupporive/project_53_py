ea456ab24a35931a8fcdba2e4089b2911cb65145
Elvira Giniatullina <elvirasupporive@gmail.com>
2025-05-08 23:46:41 -0600

    feat: add ensure login and ensure logout methods in SessionHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,11 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="first_group", header="group", footer="group"))
-    app.session.logout()
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
+
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,6 +2,4 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstName="Updated", bmonth = "May"))
-    app.session.logout()
