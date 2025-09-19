4eebfdc8657812e1b500c9eb4bd2ed55ac0a88a4
Elvira Giniatullina <elvirasupporive@gmail.com>
2025-09-18 22:42:42 -0600

    add --browser and --baseUrl options

--- conftest.py
+++ conftest.py
@@ -6,11 +6,15 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
+
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -21,3 +25,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action='store', default='firefox')
+    parser.addoption("--baseUrl", action='store', default='http://macbook-pro-elvira.local/addressbook/index.php')
