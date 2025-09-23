1ed1a6464423ddc72dee3c205095106bd74914ad
Elvira Giniatullina <elvirasupporive@gmail.com>
2025-09-23 14:47:20 -0600

    add configuration file

--- conftest.py
+++ conftest.py
@@ -1,21 +1,26 @@
+# "baseUrl": "http://macbook-pro-elvira.local/addressbook/index.php"
+
 import pytest
+import json
+import os.path
 from fixture.application import Application
 
 fixture = None
-
+target = None
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
 
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
 
-    fixture.session.ensure_login(username="admin", password="secret")
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -29,4 +34,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action='store', default='firefox')
-    parser.addoption("--baseUrl", action='store', default='http://macbook-pro-elvira.local/addressbook/index.php')
+    parser.addoption("--target", action='store', default='target.json')
