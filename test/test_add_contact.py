11283f34ae0e6551c6cc91dd7225bd24aa83733c
Elvira Giniatullina <elvirasupporive@gmail.com>
2025-09-27 13:21:21 -0600

    Add pytest_generate_tests hook for dynamic test generation

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-from generator.contact import testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
