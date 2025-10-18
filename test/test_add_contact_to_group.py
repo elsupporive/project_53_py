ea6427efb09542fcf34e1f0bb1d070b5e76a5641
Elvira Giniatullina <elvirasupporive@gmail.com>
2025-10-18 13:49:30 -0600

    fix test_add_contact_to_group

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -12,12 +12,13 @@ def test_add_contact_to_group(app, orm):
     groups = orm.get_group_list()
     group = random.choice(groups)
     old_contacts_in_group = orm.get_contacts_in_group(Group(id=group.id))
-    contacts = orm.get_contact_list()
-    contact = random.choice(contacts)
-    while contact in old_contacts_in_group:
-        contact = random.choice(contacts)
+    contacts_not_in_group = orm.get_contacts_not_in_group(group)
+    if not contacts_not_in_group:
+        app.contact.create(Contact(firstName="extra_contact"))
+        contacts_not_in_group = orm.get_contacts_not_in_group(group)
+    contact = random.choice(contacts_not_in_group)
     app.contact.add_contact_to_the_group(contact.id, group.id)
     old_contacts_in_group.append(contact)
-    new_groups = orm.get_contacts_in_group(Group(id=group.id))
-    assert sorted(old_contacts_in_group, key=Contact.id_or_max) == sorted(new_groups, key=Contact.id_or_max)
+    new_contacts_in_groups = orm.get_contacts_in_group(Group(id=group.id))
+    assert sorted(old_contacts_in_group, key=Contact.id_or_max) == sorted(new_contacts_in_groups, key=Contact.id_or_max)
 
